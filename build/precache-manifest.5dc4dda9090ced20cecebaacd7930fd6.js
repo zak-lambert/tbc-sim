3dcca9fac9cbf3acceb7b290f34d06100b0b188b
zak-lambert <zak-lambert@users.noreply.github.com>
2021-04-30 00:03:52 -0400

    wtf

---
+++ build/precache-manifest.5dc4dda9090ced20cecebaacd7930fd6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9b48fb7ed173ad775cd131fe908dc71a",
+    "url": "/tbc-sim/index.html"
+  },
+  {
+    "revision": "1c1f8adc31bb44cb089c",
+    "url": "/tbc-sim/static/css/main.1cd6dc54.chunk.css"
+  },
+  {
+    "revision": "6edc2406895595b64b0c",
+    "url": "/tbc-sim/static/js/2.8c6c6537.chunk.js"
+  },
+  {
+    "revision": "1c1f8adc31bb44cb089c",
+    "url": "/tbc-sim/static/js/main.1fae9a2b.chunk.js"
+  },
+  {
+    "revision": "a1b543e8e9d7817a7553",
+    "url": "/tbc-sim/static/js/runtime~main.0677849a.js"
+  }
+]);
